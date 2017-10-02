3feb636737534456f7a93009cefe5ff7bbac9c03
David G <david.garcia@scmspain.com>
2017-10-02 15:25:28 +0200

    Ejercicio Calculadora SUMA

---
+++ README.md
@@ -0,0 +1,8 @@
+## Calculadora JavaScript
+
+En este ejercicio pedimos 2 valores numéricos al usuario mediante la función [prompt()](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt) realizaremos una suma y devolveremos el resultado por pantalla y en la consola de JavaScript.
+
+### Ejercicio:
+- Cambia la operación aritmética de SUMA a MULTIPLICACIÓN.
+- Añade todos los cambios necesarios y haz un `commit` con el mensaje: 'refactor de función SUMA a MULTIPLICACION'
+- La calculadora ahora ha de Multiplicar los valores introducidos por el usuario y mostrar el resultado tanto en la consola de JavaScript como en Pantalla.
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+
+// Declaramos una constante llamada VALUE1 y mediante la función PROMPT obtenemos
+// el valor que introduce el usuario y lo almacenamos en dicha constante VALUE1
+const value1 = Number(window.prompt('Introduce operando 1'))
+// El resultado lo enviamos a la Consola de JavasCript del Navegador web
+// Instrucciones para abrir la Consola:
+// https://developers.google.com/web/tools/chrome-devtools/console/?hl=es
+console.log('Operando 1 es:', value1)
+
+// Declaramos la segunda constante llamada VALUE2 y almacenamos el segundo operador
+// que obtenemos del usuario también utilizando la función PROMPT
+const value2 = Number(window.prompt('Introduce operando 2'))
+// Mostramos el valor obtenido por consola
+console.log('Operando 2 es:', value2)
+
+// Declaramos una Función que obtiene 2 parámetros y la almacenamos en una constante llamada SUMA
 const suma = function (a, b) {
+    // Almacenamos en una constante llamada RESULT la suma de los parámetros A y B
     const result = a + b
+    // Enviamos el Valor contenido en RESULT a la Consola
     console.log("El resultado de la suma es:", result)
+    // Cuando se invoca esta función devuelve el valor almacenado en la constante RESULT
+    return result
 }
 
-const multiplicacion = function (a, b) {
-    const result = a * b 
-    console.log("El resultado de la multiplicacion es:", result)
-}
+// En esta constante almacenamos el resultado que retorna la ejecución de la función SUMA
+// Le pasamos como parámetros los valores VALUE1 y VALUE2 obtenidos del Usuario mediante PROMPT
+const result = suma(value1, value2)
 
-suma(12, 24)
-
-multiplicacion(5, 25)
+// Enviamos el resultado que retorna la función SUMA al elemento INPUT que se encuentra en pantalla
+window.document.getElementById("result").value = result
